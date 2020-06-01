f541a47371bfc9b474252f606dedf77ac04aa0b0
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-06-01 17:08:17 -0400

    documented project source some more and added stg_story model

---
+++ models/stg_asana_story.sql
@@ -0,0 +1,20 @@
+with story as (
+
+    select *
+    from {{ var('story') }}
+
+), fields as (
+
+    select
+      id as story_id,
+      created_at,
+      created_by_id as created_by_user_id,
+      target_id as target_task_id,
+      text as story_description,
+      type as story_type
+
+    from story
+)
+
+select *
+from fields
