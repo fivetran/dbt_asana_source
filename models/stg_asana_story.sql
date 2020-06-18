3ca35e9a6b7cb54d05fb2d3e6577bc97c22e03bb
fivetran-kristin <kristin@fivetran.com>
2020-06-18 12:05:39 -0700

    minor edits

--- models/stg_asana_story.sql
+++ models/stg_asana_story.sql
@@ -10,7 +10,7 @@ with story as (
       created_at,
       created_by_id as created_by_user_id,
       target_id as target_task_id,
-      text as story_content, -- I like "content" for this. I feels like a term that can be used both for the system generated logs, as well as user text
+      text as story_content,
       type as event_type
 
     from story
