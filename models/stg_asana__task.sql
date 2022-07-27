75d45891dba8513c1375a150e6ab8d4f6cff1b70
Poonam Agate <poonam.agate@fivetran.com>
2022-07-27 18:06:02 +0100

    made changes to relevant files for removing deprecated fields

--- models/stg_asana__task.sql
+++ models/stg_asana__task.sql
@@ -31,7 +31,6 @@ final as (
     select 
         id as task_id,
         assignee_id as assignee_user_id,
-        assignee_status,
         completed as is_completed,
         cast(completed_at as {{ dbt_utils.type_timestamp() }}) as completed_at,
         completed_by_id as completed_by_user_id,
