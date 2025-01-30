8821348954b0a22b1e7fb7e134212833e5d839c8
Avinash Kunnath <avinash.kunnath@fivetran.com>
2025-01-30 01:45:23 -0800

    Does table exist macro

--- models/tmp/stg_asana__tag_tmp.sql
+++ models/tmp/stg_asana__tag_tmp.sql
@@ -1,2 +1,14 @@
+{% if var('using_asana__tag', asana_source.does_table_exist('tag')) %}
+
 select * 
 from {{ var('tag') }}
+
+{% else %}
+
+select 
+    cast(null as {{ dbt.type_string() }}) as id,
+    cast(null as {{ dbt.type_string() }}) as name,
+    cast(null as {{ dbt.type_timestamp() }}) as created_at
+limit {{ '1' if target.type == 'redshift' else '0' }} 
+
+{% endif %}
--- models/tmp/stg_asana__task_tag_tmp.sql
+++ models/tmp/stg_asana__task_tag_tmp.sql
@@ -1,2 +1,13 @@
+{% if var('using_asana__task_tag', asana_source.does_table_exist('task_tag')) %}
+
 select * 
 from {{ var('task_tag') }}
+
+{% else %}
+
+select 
+    cast(null as {{ dbt.type_string() }}) as tag_id,
+    cast(null as {{ dbt.type_string() }}) as task_id
+limit {{ '1' if target.type == 'redshift' else '0' }} 
+
+{% endif %}
