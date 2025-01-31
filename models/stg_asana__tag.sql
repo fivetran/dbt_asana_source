409db8e7e38df2271df3d3442f14f5c5ab924e61
Avinash Kunnath <avinash.kunnath@fivetran.com>
2025-01-31 08:48:28 -0800

    remove conditional block

--- models/stg_asana__tag.sql
+++ models/stg_asana__tag.sql
@@ -1,5 +1,3 @@
-{% if var('using_asana_tags', asana_source.does_table_exist('tag')) %}
-
 with base as (
 
     select * 
@@ -32,13 +30,3 @@ final as (
 
 select * 
 from final
-
-{% else %}
-
-select 
-    cast(null as {{ dbt.type_string() }}) as id,
-    cast(null as {{ dbt.type_string() }}) as name,
-    cast(null as {{ dbt.type_timestamp() }}) as created_at
-limit {{ '1' if target.type == 'redshift' else '0' }} 
-
-{% endif %}
